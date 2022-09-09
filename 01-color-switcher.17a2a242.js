fbe42d729dccbee52dc90fa8e8b0142b30e16fa7
gradussus <103496948+gradussus@users.noreply.github.com>
2022-09-09 19:50:15 +0000

    Deploying to gh-pages from @ gradussus/goit-js-hw-09@e07fbc471c5da553250584a30f24cee1f5a77fd9 🚀

--- 01-color-switcher.17a2a242.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){console.log("lkjahsdfl;khasdflk;h")})),t.addEventListener("click",(function(){console.log("lkjahsdfl;khasdflk;h")}));
-//# sourceMappingURL=01-color-switcher.17a2a242.js.map
--- 01-color-switcher.b0484a44.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){console.log("lkjahsdfl;khasdflk;h")})),t.addEventListener("click",(function(){console.log("lkjahsdfl;khasdflk;h")}))}();
-//# sourceMappingURL=01-color-switcher.b0484a44.js.map
