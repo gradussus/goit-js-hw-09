e07fbc471c5da553250584a30f24cee1f5a77fd9
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-09-09 22:49:37 +0300

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,30 @@
 const startBnt = document.querySelector('[data-start]');
 const stopBnt = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+stopBnt.disabled = true;
 
 startBnt.addEventListener('click', startFn);
 stopBnt.addEventListener('click', stopFn);
 
-function startFn() {
-    console.log ("lkjahsdfl;khasdflk;h")
-}
-function stopFn() {
-    console.log ("lkjahsdfl;khasdflk;h")
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+function randomBodyBgcColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function startFn() {
+  randomBodyBgcColor();
+  timer = setInterval(() => {
+    randomBodyBgcColor();
+  }, 1000);
+  stopBnt.disabled = false;
+  startBnt.disabled = true;
+}
+
+function stopFn() {
+  startBnt.disabled = false;
+  stopBnt.disabled = true;
+  clearInterval(timer);
+}
