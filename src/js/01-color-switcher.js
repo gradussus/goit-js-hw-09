1918c0021d5c9b5f722396e3e914bbcaa8d2afdb
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-09-09 17:55:23 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const startBnt = document.querySelector('[data-start]');
+const stopBnt = document.querySelector('[data-stop]');
 
+startBnt.addEventListener('click', startFn);
+stopBnt.addEventListener('click', stopFn);
+
+function startFn() {
+    console.log ("lkjahsdfl;khasdflk;h")
+}
+function stopFn() {
+    console.log ("lkjahsdfl;khasdflk;h")
+}
